1faac1b260b3d68745b0e395781c7e90bd307a48
Joe Baranoski <jbrnsk@gmail.com>
2024-05-20 11:35:26 -0500

    Remove extraneous boilerplate

--- apps/game/src/main.ts
+++ apps/game/src/main.ts
@@ -1,9 +1,10 @@
-import { createApp } from 'vue';
-import App from './App.vue'
-
-
 import './styles/main.css'
+
+import { createApp } from 'vue';
+
 import router from '@/router';
+
+import App from './App.vue'
 
 
 const app = createApp(App)
