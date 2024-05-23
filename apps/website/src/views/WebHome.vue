666f8407b704c7b101fee91b628e76d7ad835781
Joe Baranoski <jbrnsk@gmail.com>
2024-05-23 13:42:42 -0500

    Implement start heading styles

--- apps/website/src/views/WebHome.vue
+++ apps/website/src/views/WebHome.vue
@@ -5,7 +5,7 @@
 <template>
   <div class="flex h-full items-center justify-center">
     <span
-      class="font-cyber drop-shadow-blue-700 text-cyber-pink absolute z-10 text-5xl"
+      class="shimmer font-cyber text-cyber-pink text-shadow absolute z-10 text-5xl"
     >
       Cyberbowl 3090
     </span>
@@ -13,4 +13,95 @@
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+  .shimmer {
+    padding-right: 140px;
+    margin-right: -140px;
+    text-shadow:
+      1px 1px 0 theme('colors.cyber.blue'),
+      4px 4px 0 white,
+      4px 4px 0.2em theme('colors.cyber.pink');
+  }
+
+  .shimmer {
+    color: rgba(255, 255, 255, 0.1);
+    background: -webkit-gradient(
+      linear,
+      left top,
+      right top,
+      from(#222),
+      to(#222),
+      color-stop(0.5, #fff)
+    );
+    background: -moz-gradient(
+      linear,
+      left top,
+      right top,
+      from(#222),
+      to(#222),
+      color-stop(0.5, #fff)
+    );
+    background: gradient(
+      linear,
+      left top,
+      right top,
+      from(#222),
+      to(#222),
+      color-stop(0.5, #fff)
+    );
+    -webkit-background-size: 125px 100%;
+    -moz-background-size: 125px 100%;
+    background-size: 125px 100%;
+    -webkit-background-clip: text;
+    -moz-background-clip: text;
+    background-clip: text;
+    -webkit-animation-name: shimmer;
+    -moz-animation-name: shimmer;
+    animation-name: shimmer;
+    -webkit-animation-duration: 2s;
+    -moz-animation-duration: 2s;
+    animation-duration: 2s;
+    -webkit-animation-iteration-count: infinite;
+    -moz-animation-iteration-count: infinite;
+    animation-iteration-count: infinite;
+    background-repeat: no-repeat;
+    background-position: 0 0;
+    background-color: #222;
+  }
+
+  @-moz-keyframes shimmer {
+    0% {
+      background-position: top left;
+    }
+    100% {
+      background-position: top right;
+    }
+  }
+
+  @-webkit-keyframes shimmer {
+    0% {
+      background-position: top left;
+    }
+    100% {
+      background-position: top right;
+    }
+  }
+
+  @-o-keyframes shimmer {
+    0% {
+      background-position: top left;
+    }
+    100% {
+      background-position: top right;
+    }
+  }
+
+  @keyframes shimmer {
+    0% {
+      background-position: top left;
+    }
+    100% {
+      background-position: top right;
+    }
+  }
+</style>
